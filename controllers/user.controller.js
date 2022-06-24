e95e78d87b288f397da43cb84436ce42efc82d95
Kirill <freshjs.ftd.@gmail.com>
2022-06-24 20:10:19 +0300

    Add find methods

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,8 +6,48 @@ module.exports.createUser = async (req, res, next) => {
 
     const user = await User.create(body);
 
+    user.password = undefined;
+
     res.send({ data: user });
   } catch (error) {
     next(error);
   }
 };
+
+module.exports.findUsers = async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: {
+        exclude: ['password'],
+      },
+      where: { firstName: 'Test' },
+    });
+
+    // const users = await User.findAll({
+    //   attributes: ['email', 'firstName', 'lastName']
+    // });
+
+    res.send({ data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.findUserById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const user = await User.findByPk(id, {
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+
+    res.send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
--- routers/index.js
+++ routers/index.js
@@ -7,16 +7,10 @@ const UserController = require('../controllers/user.controller');
 /* localhost:5000/api/users */
 router.post('/users', UserController.createUser);
 /* localhost:5000/api/users */
-router.get('/users', () => {});
+router.get('/users', UserController.findUsers);
 
 /* http://localhost:5000/api/users/5 */
-router.get('/users/:id', (req, res, next) => {
-  const {
-    params: { id },
-  } = req;
-
-  res.send(id);
-});
+router.get('/users/:id', UserController.findUserById);
 
 router.post('/users', (req, res, next) => {
   const { body } = req;
