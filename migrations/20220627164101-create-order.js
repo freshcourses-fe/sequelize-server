8e3fe5e2ddedadd9f3ee88d5caf13424a1268465
Kirill <freshjs.ftd.@gmail.com>
2022-06-27 19:54:40 +0300

    Add cascade ot orders migration

--- migrations/20220627164101-create-order.js
+++ migrations/20220627164101-create-order.js
@@ -17,6 +17,8 @@ module.exports = {
             tableName: 'users',
           },
           key: 'id',
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
         },
       },
       createdAt: {
